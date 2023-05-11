21db723baebb27624d71090a4e9df8e8566f9655
m4loo <128167781+m4loo@users.noreply.github.com>
2023-05-12 06:40:36 +0700

    done

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int result;
+        if ((numberToBeRounded*10) % 10 < 5) {
+            result = (int) numberToBeRounded;
+        } else result = (int) numberToBeRounded + 1;
     }
 }
